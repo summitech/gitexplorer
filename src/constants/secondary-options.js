7ac4ec266d8809d143ca1ffc8a0e0a9fdaf145d3
FatimahSanni <fatimah.sanni@gmail.com>
2019-01-08 15:55:27 +0100

    chore(app): Add more git commands

--- src/constants/secondary-options.js
+++ src/constants/secondary-options.js
@@ -61,9 +61,7 @@ export const secondaryOptions = {
   show: [
     {
       value: 'logs',
-      label: 'commit logs/history',
-      usage: 'git log',
-      nb: 'Type q in the terminal to exit the logs'
+      label: 'commit logs/history'
     },
     {
       value: 'uncommittedChanges',
@@ -75,25 +73,51 @@ export const secondaryOptions = {
       label: 'committed/staged changes',
       usage: 'git diff --staged'
     },
+    {
+      value: 'remoteUrl',
+      label: 'repo\'s remote url',
+      usage: 'git remote -v'
+    },
+    {
+      value: 'stash',
+      label: 'stash',
+      usage: 'git stash list'
+    },
+    {
+      value: 'branch',
+      label: 'branches',
+      usage: 'git branch',
+      nb: 'The active branch is prefixed with *'
+    }
   ],
   delete: [
     {
       value: 'branch',
       label: 'a branch',
-      usage: 'git branch -D branch name'
+      usage: 'git branch -D <branch name>'
+    },
+    {
+      value: 'remote',
+      label: 'remote',
+      usage: 'git remote rm <remote>'
+    },
+    {
+      value: 'stash',
+      label: 'stash',
+      usage: 'git stash clear'
     }
   ],
   compareCommits: [
     {
       value: 'terminal',
       label: 'and output result in the terminal',
-      usage: 'git diff sha1 sha2 | less',
+      usage: 'git diff <sha1> <sha2> | less',
       nb: 'sha1 and sha2 are the sha hash of the commits you want to compare.',
     },
     {
       value: 'file',
       label: 'and output result to a file',
-      usage: 'git diff sha1 sha2 > diff.txt',
+      usage: 'git diff <sha1> <sha2> > diff.txt',
       nb: 'sha1 and sha2 are the sha of the commits you want to compare. \n\ndiff.txt is the file you want to store the contents of the diff',
     }
   ],
@@ -109,15 +133,49 @@ export const secondaryOptions = {
     {
       value: 'clone-repo',
       label: 'existing repo',
-      usage: 'git clone repo-url'
+      usage: 'git clone <repo-url>'
     }
   ],
   ignore: [
     {
       value: 'ignore-files-in-a-dir',
       label: 'all files in a directory',
-      usage: 'dir name/*',
+      usage: '<dir name>/*',
       nb: 'This must be added to .gitignore file\n\nReplace "dir name" with name of directory whose files you want git to ignore'
     }
   ],
+  help: [
+    {
+      value: 'command-help',
+      label: 'about a command',
+      usage: 'append --help to the command',
+      nb: 'e.g. git merge --help\n\nType q to quite terminal'
+    }
+  ],
+  add: [
+    {
+      value: 'add-repo',
+      label: 'new remote repo',
+      usage: 'git remote add <shortname> <url>'
+    },
+    {
+      value: 'add-alias',
+      label: 'alias',
+      usage: 'git config --global alias.<alias> <command>',
+      nb: 'e.g. git config --global alias.st status. Typing git st in the terminal now does the same thing as git status'
+    }
+  ],
+  push: [
+    {
+      value: 'new-remote-branch',
+      label: 'non-existent remote branch',
+      usage: 'git push -u origin <branchname>'
+    }
+  ],
+  rename: [
+    {
+      value: 'branch',
+      label: 'branch'
+    }
+  ]
 };
--- src/constants/tertiary-options.js
+++ src/constants/tertiary-options.js
@@ -1,1 +1,34 @@
-export const tertiaryOptions = {};
+export const tertiaryOptions = {
+  logs: [
+    {
+      value: 'all',
+      label: 'all',
+      usage: 'git log',
+      nb: 'Type q in the terminal to exit the logs'
+    },
+    {
+      value: 'last-n-commit',
+      label: 'for last xxx number of commits',
+      usage: 'git log -n',
+      nb: 'Replace n with number of commits e.g. git log -2'
+    },
+    {
+      value: 'particular-period',
+      label: 'since a particular period',
+      usage: 'git log --since=period',
+      nb: 'Replace period with intended timeframe e.g git log --since=3days. You can use dates like 2018-12-31.\n\n Similar flags are --until, --before, --after'
+    }
+  ],
+  branch: [
+    {
+      value: 'in',
+      label: 'while working in the branch',
+      usage: 'git branch -m <new name>'
+    },
+    {
+      value: 'outside',
+      label: 'from outside the branch',
+      usage: 'git branch -m <old name> <new name>'
+    }
+  ]
+};
