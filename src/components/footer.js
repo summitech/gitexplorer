f7fd4d649a5e31b109f15e99391acc1964a367ab
FatimahSanni <fatimah.sanni@gmail.com>
2019-01-12 14:56:38 +0100

    feat(footer): Add github link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import github from 'assets/images/github.svg';
+import githubGreen from 'assets/images/github-green.svg';
 
-const Footer = () => (
+function openRepo() {
+  window.open('https://github.com/summitech/gitexplorer', '_blank');
+}
+
+const Footer = props => (
   <footer className="footer">
+  <div className="logo">
+      {props.dark ? (
+        <img src={githubGreen} alt="Github Logo" className="logo--github" onClick={() => openRepo()} />
+      ) : (
+        <img src={github} alt="Github Logo" className="logo--github" onClick={() => openRepo()} />
+      )}
+    </div>
     <p className="footer__copyright dark-white">
       Made with <span>❤</span> by{' '}
       <a href="https://www.summitech.ng" target="_blank" rel="noopener noreferrer">
