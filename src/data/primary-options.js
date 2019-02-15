4ebc9fac5c4afcecf1b5af6d8f2151d09eece884
SomeshThakur <someshthakur33@gmail.com>
2019-02-15 21:48:31 +0530

    Improve PrimaryOption lambda function :)

--- src/data/primary-options.js
+++ src/data/primary-options.js
@@ -19,11 +19,5 @@ const options = [
 ];
 
 export const primaryOptions = options.sort((x, y) => {
-  if (x.value < y.value) {
-    return -1;
-  }
-  if (x.value > y.value) {
-    return 1;
-  }
-  return 0;
+  return x.value - y.value;
 });
