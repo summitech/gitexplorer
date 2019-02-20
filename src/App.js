471e0035ce03607585f76bc0bd2bb879aa0e540c
Arthur Denner <arthurdenner7@gmail.com>
2019-02-20 15:10:56 -0300

    Fix ESLint errors

--- src/App.js
+++ src/App.js
@@ -107,7 +107,9 @@ class App extends Component {
     el.style.position = 'absolute';
     el.style.left = '-9999px';
     document.body.appendChild(el);
-    const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    const selected = document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
