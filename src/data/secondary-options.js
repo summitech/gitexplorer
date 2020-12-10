2773274c709940b7efae1933ee653f0267b0389c
msdolbey <dolbes@gmail.com>
2020-12-10 13:00:18 -0600

    adding the primary commands

--- src/data/secondary-options.js
+++ src/data/secondary-options.js
@@ -1,16 +1,74 @@
 export const secondaryOptions = {
-  up: [
+
+  apply: [
     {
       value: 'yes',
       label: 'without further prompts',
-      usage: 'konvoy up -y'
-    },
+      usage: 'konvoy apply',
+    }
   ],
-  init: [
+
+  create: [
     {
       value: 'yes',
       label: 'without further prompts',
-      usage: 'konvoy init',
+      usage: 'konvoy create',
+    }
+  ],
+
+  cordon: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy cordon',
+    }
+  ],
+
+  config: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy config',
+    }
+  ],
+
+  completion: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy completion',
+    }
+  ],
+
+  check: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy check',
+    }
+  ],
+
+  diff: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy diff',
+    }
+  ],
+
+  diagnose: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy diagnose -y',
+    }
+  ],
+
+  deploy: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy deploy -y',
     }
   ],
 
@@ -19,6 +77,116 @@ export const secondaryOptions = {
       value: 'yes',
       label: 'without further prompts',
       usage: 'konvoy destroy -y',
+    }
+  ],
+
+  drain: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy drain -y',
+    }
+  ],
+
+  get: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy get',
+    }
+  ],
+
+  generate: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy generate',
+    }
+  ],
+
+  generate: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy image',
+    }
+  ],
+
+  image: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy image',
+    }
+  ],
+
+  init: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy init',
+    }
+  ],
+
+  provision: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy provision',
+    }
+  ],
+   
+  pull: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy pull',
+    }
+  ],
+
+  push: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy push',
+    }
+  ],
+
+  reset: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy reset',
+    }
+  ],
+
+  scale: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy scale',
+    }
+  ],
+  uncordon: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy uncordon',
+    }
+  ],
+  up: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy up -y'
+    },
+  ],
+  
+  view: [
+    {
+      value: 'yes',
+      label: 'without further prompts',
+      usage: 'konvoy view',
     }
   ],
 };
