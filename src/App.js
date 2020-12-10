0e2585b8f6ae30fc6adb1022ee44f3205881a01f
Rodney Norris <rnorris@d2iq.com>
2020-12-10 12:22:57 -0600

    chore: s/Git/Konvoy/g

--- src/App.js
+++ src/App.js
@@ -149,7 +149,7 @@ class App extends Component {
             <div className="row">
               <div className="col-5">
                 <h2 className="content__title  dark-white">
-                  Git <span>Command</span> Explorer
+                  Konvoy <span>Command</span> Explorer
                 </h2>
                 <p className="content__subtitle dark-grey">
                   Find the right commands you need without digging through the web.
