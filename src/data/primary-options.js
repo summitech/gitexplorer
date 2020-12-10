0f983fe41976c100d775dd2a8bb8e816b19f6166
natmegs <nataliemegans@gmail.com>
2020-12-10 09:31:36 -0800

    feat: add konvoy up command

--- src/data/primary-options.js
+++ src/data/primary-options.js
@@ -1,24 +1,5 @@
 const options = [
-  { value: 'add', label: 'add' },
-  { value: 'commit', label: 'commit' },
-  { value: 'revert', label: 'revert/reset' },
-  { value: 'initialize', label: 'initialize' },
-  { value: 'modify', label: 'modify' },
-  { value: 'show', label: 'show/view' },
-  { value: 'delete', label: 'delete/remove' },
-  { value: 'compareCommits', label: 'compare two commits' },
-  { value: 'configure', label: 'configure' },
-  { value: 'clone', label: 'clone' },
-  { value: 'ignore', label: 'ignore' },
-  { value: 'rename', label: 'rename' },
-  { value: 'merge', label: 'merge' },
-  { value: 'squash', label: 'squash' },
-  { value: 'stash', label: 'stash' },
-  { value: 'debug', label: 'debug' },
-  { value: 'recover', label: 'recover' },
-  { value: 'synchronize', label: 'synchronize' },
-  { value: 'rebase', label: 'rebase' },
-  { value: 'cherrypick', label: 'cherry-pick' },
+  { value: 'up', label: 'provision a cluster' },
 ];
 
 export const primaryOptions = options.sort((x, y) => {
